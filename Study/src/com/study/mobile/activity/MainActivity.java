d66c55de157edbf7c2fecd0fd6a7f3417ec203c9
zhangwy <zhangwy@fun.tv>
2016-02-14 16:30:00 +0800

    首页列表

--- Study/src/com/study/mobile/activity/MainActivity.java
+++ Study/src/com/study/mobile/activity/MainActivity.java
@@ -1,20 +1,37 @@
 package com.study.mobile.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.study.mobile.R;
+import com.study.mobile.entity.ActionEntity;
+import com.zwy.widget.adapter.BaseAdapter;
+import com.zwy.widget.adapter.BaseAdapter.OnItemLoading;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements OnItemClickListener, OnItemLoading<ActionEntity>{
 
+	private TextView desc;
+	private ListView list;
+	private BaseAdapter<ActionEntity> mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
     }
 
-    @Override
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -28,4 +45,31 @@ public class MainActivity extends BaseActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	private void initView() {
+		desc = (TextView) findViewById(R.id.main_desc);
+		list = (ListView) findViewById(R.id.main_list);
+	}
+
+	private void initData() {
+		desc.setText(R.string.desc_main);
+		mAdapter = new BaseAdapter<ActionEntity>(getItems(), this);
+		list.setAdapter(mAdapter);
+	}
+
+	private List<ActionEntity> getItems(){
+		List<ActionEntity> list = new ArrayList<ActionEntity>();
+		list.add(new ActionEntity(list.size(), getString(R.string.name_cycle), getString(R.string.desc_cycle), CycleActivity.class));
+		return list;
+	}
+
+	@Override
+	public View getView(View view, ActionEntity item) {
+		return null;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		startActivity(new Intent(this, mAdapter.getItem(position).getClass()));
+	}
 }
