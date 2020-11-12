d9e2bc5e9b3e5c3dea4ca937779cc41eece53ef6
zhangweiya <zhangweiya@autohome.com.cn>
2020-11-12 16:41:05 +0800

    获取传感器

--- sample/app/src/main/java/com/zhangwy/sample/ui/PermissionActivity.java
+++ sample/app/src/main/java/com/zhangwy/sample/ui/PermissionActivity.java
@@ -3,15 +3,24 @@ package com.zhangwy.sample.ui;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.zhangwy.sample.R;
 import com.zhangwy.util.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class PermissionActivity extends BaseActivity {
 
@@ -23,6 +32,7 @@ public class PermissionActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
         this.refreshVolume();
+        this.refreshSensor();
     }
 
     private void refreshVolume() {
@@ -82,5 +92,80 @@ public class PermissionActivity extends BaseActivity {
         this.findViewById(R.id.permissionAccessNotificationPolicyVolumeIncrease).setEnabled(!(ringerMode == AudioManager.RINGER_MODE_VIBRATE));
         this.findViewById(R.id.permissionAccessNotificationPolicyVolumeReduce).setEnabled(!(ringerMode == AudioManager.RINGER_MODE_VIBRATE));
         this.findViewById(R.id.permissionAccessNotificationPolicyVolumeRefresh).performClick();
+    }
+
+    private void refreshSensor() {
+        final SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        final TextView allText = this.findViewById(R.id.permissionSensorsText);
+        this.findViewById(R.id.permissionSensorsRefresh).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (manager == null) {
+                    return;
+                }
+                List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ALL);
+                StringBuilder builder = new StringBuilder();
+                builder.append(getString(R.string.sensor_list));
+                builder.append(System.getProperty("line.separator"));
+                JSONArray array = new JSONArray();
+                for (Sensor sensor : sensors) {
+                    JSONObject json = new JSONObject();
+                    try {
+                        json.put("name", sensor.getName());
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            json.put("id", sensor.getId());
+                            json.put("SType", sensor.getStringType());
+                        }
+                        json.put("type", sensor.getType());
+                        json.put("resolution", sensor.getResolution());
+                        json.put("Ventor", sensor.getVendor());
+                        json.put("Version", sensor.getVersion());
+                        json.put("power", sensor.getPower());
+                    } catch (JSONException e) {
+                        Logger.e("", e);
+                    }
+                    array.put(json);
+                }
+                builder.append(array.toString());
+                allText.setText(builder.toString());
+            }
+        });
+        this.findViewById(R.id.permissionSensorsRefresh).performClick();
+        final TextView typeSensor = this.findViewById(R.id.permissionSensorTypeText);
+        final EditText typeInput = this.findViewById(R.id.permissionSensorTypeInput);
+        this.findViewById(R.id.permissionSensorTypeRefresh).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (manager == null) {
+                    return;
+                }
+                String typeString = typeInput.getText().toString();
+                Sensor sensor = manager.getDefaultSensor(Integer.parseInt(typeString));
+                StringBuilder builder = new StringBuilder();
+                builder.append(getString(R.string.sensor_type));
+                if (sensor == null) {
+                    typeSensor.setText(builder.toString());
+                    return;
+                }
+                builder.append(System.getProperty("line.separator"));
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("name", sensor.getName());
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        json.put("id", sensor.getId());
+                        json.put("SType", sensor.getStringType());
+                    }
+                    json.put("type", sensor.getType());
+                    json.put("resolution", sensor.getResolution());
+                    json.put("Ventor", sensor.getVendor());
+                    json.put("Version", sensor.getVersion());
+                    json.put("power", sensor.getPower());
+                } catch (JSONException e) {
+                    Logger.e("", e);
+                }
+                builder.append(json.toString());
+                typeSensor.setText(builder.toString());
+            }
+        });
     }
 }
